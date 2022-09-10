776ca04bc7f87db469f4a741dfec2ccf489846c5
elielaurentraphael <raphelaur@gmail.com>
2022-09-11 01:24:10 +0300

    Commit n°5 - Top bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
   products = [
     {
-      name:'Phone XL'
+      name: 'Phone XL',
+      price: 799,
+      description: 'A large phone with one of the best screens'
     },
     {
-      name:'Phone Mini'
+      name: 'Phone Mini',
+      price: 699,
+      description: 'A great phone with one of the best cameras'
     },
     {
-      name:'Phone Standart'
+      name: 'Phone Standard',
+      price: 299,
+      description: ''
     }
   ];
-
 }
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -7,8 +7,8 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  @Input()
-  productName!: string;
+  @Input() productName! : string;
+  @Input() productDescription! : string;
 
   constructor() { }
 
